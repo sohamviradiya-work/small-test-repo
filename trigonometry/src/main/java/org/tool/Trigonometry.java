2319ed33c512945ff341afeaa9138c03f4b70152
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-05 14:50:31 +0530

    fail area test

--- trigonometry/src/main/java/org/tool/Trigonometry.java
+++ trigonometry/src/main/java/org/tool/Trigonometry.java
@@ -15,7 +15,7 @@ public class Trigonometry {
 
     public static double areaOfTriangle(double side1, double side2, double side3) {
         double s = (side1 + side2 + side3) / 2;
-        double area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        double area = Math.sqrt(s * (s - side1) * (s - side2) * (s + side3));
         return area;
     }
     
