eb40ef14400e87aa6392dbb7367d84e167b0b869
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-06-05 14:54:22 +0530

    fail median test

--- statistics/src/main/java/org/basic/BasicStatistics.java
+++ statistics/src/main/java/org/basic/BasicStatistics.java
@@ -18,7 +18,7 @@ public class BasicStatistics {
         Arrays.sort(numbers);
         int n = numbers.length;
         if (n % 2 == 0) {
-            return (numbers[n / 2 - 1] + numbers[n / 2]) / 2.0;
+            return (numbers[n / 2 - 1] + numbers[n / 2]);
         } else {
             return numbers[n / 2];
         }
