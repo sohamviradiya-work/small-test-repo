6dc297e185b2e4b0699f45427ad899da76ce82d8
sohamviradiya.work@gmail.com <sohamviradiya.work@gmail.com>
2024-07-03 12:19:09 +0530

    change test

--- series/src/test/java/org/tool/SeriesTest.java
+++ series/src/test/java/org/tool/SeriesTest.java
@@ -8,7 +8,7 @@ public class SeriesTest {
     @Test
     public void testArithmetic() {
         Series series = new Series();
-        assertEquals(18.0, series.arithmetic(1, 3, 4), 0.0001);
+        assertEquals(22.0, series.arithmetic(1, 3, 4), 0.0001);
     }
 
     @Test
